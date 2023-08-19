4dd5859ec04369d0cb06313f8eaa891200527464
Edgardo Rivero <edgardo.rivero@ucsm.edu.pe>
2023-08-19 17:21:41 -0500

    Codigo modificado con clase regalo

--- Principal.java
+++ Principal.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
 
 public class Principal {
-    public static void main(String[] args ) {
+    public static void main(String[] args) {
         int vida, rp;
         boolean fl = false;
         vida = 0;
         Scanner sc = new Scanner(System.in);
+        
         do {
             vida++;
-            System.out.println("Vida: "+ vida + "\tCual es el numero secreto? ");
+            System.out.println("Vida: " + vida + "\t¿Cuál es el número secreto? ");
             rp = sc.nextInt();
-            if (rp == 200)
+            
+            if (rp == 15)
                 fl = true;
-        } while(vida < 2 && !fl);
+        } while (vida < 2 && !fl);
 
-        if( fl == true)
-            System.out.println("Adivinaste!!!!");
-        else
-            System.out.println("ups, perdiste");
-    }    
+        if (fl) {
+            System.out.println("¡Adivinaste!");
+            System.out.println("Ganaste tu gran premio: " + Regalo.elegir(vida));
+        } else {
+            System.out.println("Ups, perdiste...");
+        }
+    }
 }
