97c025a07e5b848b42eeb50a4ffa5ca49a2ffe77
Edgardo Rivero <108624162+erivero96@users.noreply.github.com>
2023-11-26 23:31:59 -0500

    Create Procedencia.java

---
+++ Laboratorio11/Actividad1/Procedencia.java
@@ -0,0 +1,11 @@
+
+class Procedencia {
+    private String departamento;
+    private String provincia;
+
+    public Procedencia(String departamento, String provincia) {
+        this.departamento = departamento;
+        this.provincia = provincia;
+    }
+
+}
